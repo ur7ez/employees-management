9a7828f43190d2523a51e0a8dc01077094961c56
ur7ez <ur7ez@ukr.net>
2023-09-13 23:50:58 +0300

    remove unused class import

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use App\Tables\Users;
 use ProtoneMedia\Splade\Facades\Splade;
 
