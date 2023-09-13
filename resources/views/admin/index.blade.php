b9cf950188539810c1f02de4bbaad009c7202257
ur7ez <ur7ez@ukr.net>
2023-09-13 20:58:36 +0300

    add admin layout, navigation and sidebar components

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,3 @@
+<x-admin-layout>
+    <h1>Welcome to Admin!</h1>
+</x-admin-layout>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,12 @@
+<div class="min-h-screen bg-gray-100">
+    @include('layouts.admin-navigation')
+    <div class="flex space-x-4">
+        <Sidebar/>
+        <!-- Page Content -->
+        <main class="flex-1">
+            <div class="max-w-6xl mx-auto">
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
+</div>
