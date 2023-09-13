1573434c292cd0e6279f1419d11bfb694414e2e8
ur7ez <ur7ez@ukr.net>
2023-09-13 23:50:33 +0300

    fill up country controller CRUD methods

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreCountryRequest;
+use App\Http\Requests\UpdateCountryRequest;
+use App\Models\Country;
+use App\Tables\Countries;
+use ProtoneMedia\Splade\Facades\Splade;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use ProtoneMedia\Splade\SpladeForm;
 
 class CountryController extends Controller
 {
@@ -11,7 +18,9 @@ class CountryController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.countries.index', [
+            'countries' => Countries::class,
+        ]);
     }
 
     /**
@@ -19,46 +28,54 @@ class CountryController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.countries.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCountryRequest $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        Country::create($request->validated());
+        Splade::toast('Country created')->autoDismiss(3);
+        return to_route('admin.countries.index');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Country $country)
     {
-        //
+        $form = SpladeForm::make()
+            ->action(route('admin.countries.update', $country))
+            ->fields([
+                Input::make('country_code')->label('Country Code'),
+                Input::make('name')->label('Country Name'),
+                Submit::make()->label('Update'),
+            ])
+            ->class('p-4 bg-white rounded-md space-y-2')
+            ->method('PUT')
+            ->fill($country);
+        return view('admin.countries.edit', compact('form'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateCountryRequest $request, Country $country)
     {
-        //
+        $country->update($request->validated());
+        Splade::toast('Country updated')->autoDismiss(3);
+        return to_route('admin.countries.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Country $country)
     {
-        //
+        $country->delete();
+        Splade::toast('Country deleted')->autoDismiss(3);
+        return back();
     }
 }
