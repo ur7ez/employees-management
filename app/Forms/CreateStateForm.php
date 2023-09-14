9501af1fbcf7e7ef1399a0d3958e93c1be6dd0a5
ur7ez <ur7ez@ukr.net>
2023-09-14 13:48:17 +0300

    add index and create flow for States

---
+++ app/Forms/CreateStateForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Forms;
+
+use App\Models\Country;
+use ProtoneMedia\Splade\AbstractForm;
+use ProtoneMedia\Splade\FormBuilder\Select;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use ProtoneMedia\Splade\FormBuilder\Text;
+use ProtoneMedia\Splade\SpladeForm;
+
+class CreateStateForm extends AbstractForm
+{
+    public function configure(SpladeForm $form)
+    {
+        $form
+            ->action(route('admin.states.store'))
+            ->class('p-4 bg-white rounded-md space-y-2');
+    }
+
+    public function fields(): array
+    {
+        return [
+            Text::make('name')
+                ->label('Name'),
+            Select::make('country_id')
+                ->label('Choose a country')
+                ->options(Country::pluck('name', 'id')->toArray()),
+            Submit::make()
+                ->label('Save'),
+        ];
+    }
+}
--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Forms\CreateStateForm;
+use App\Http\Requests\UpdateStateRequest;
+use App\Models\State;
+use App\Tables\States;
 use Illuminate\Http\Request;
+use ProtoneMedia\Splade\Facades\Splade;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use ProtoneMedia\Splade\SpladeForm;
 
 class StateController extends Controller
 {
@@ -11,7 +19,9 @@ class StateController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.states.index', [
+            'states' => States::class,
+        ]);
     }
 
     /**
@@ -19,46 +29,56 @@ class StateController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.states.create', [
+            'form' => CreateStateForm::class,
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, CreateStateForm $form)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        State::create($form->validate($request));
+        Splade::toast('State created')->autoDismiss(3);
+        return to_route('admin.states.index');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(State $state)
     {
-        //
+        $form = SpladeForm::make()
+            ->action(route('admin.states.update', $state))
+            ->fields([
+                Input::make('country_id')->label('Country ID'),
+                Input::make('name')->label('State Name'),
+                Submit::make()->label('Update'),
+            ])
+            ->class('p-4 bg-white rounded-md space-y-2')
+            ->method('PUT')
+            ->fill($state);
+        return view('admin.states.edit', compact('form'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateStateRequest $request, State $state)
     {
-        //
+        $state->update($request->validated());
+        Splade::toast('State updated')->autoDismiss(3);
+        return to_route('admin.states.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(State $state)
     {
-        //
+        $state->delete();
+        Splade::toast('State deleted')->autoDismiss(3);
+        return back();
     }
 }
