9ed5df0f8264166163159abdd7b5dd6710e0125b
ur7ez <ur7ez@ukr.net>
2023-09-15 15:27:39 +0300

    add crud for departments

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateDepartmentRequest;
+use App\Models\Department;
+use App\Tables\Departments;
 use Illuminate\Http\Request;
+use ProtoneMedia\Splade\Facades\Splade;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use ProtoneMedia\Splade\SpladeForm;
 
 class DepartmentController extends Controller
 {
@@ -11,7 +18,9 @@ class DepartmentController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.departments.index', [
+            'departments' => Departments::class,
+        ]);
     }
 
     /**
@@ -19,46 +28,66 @@ class DepartmentController extends Controller
      */
     public function create()
     {
-        //
+        $form = SpladeForm::make()
+            ->action(route('admin.departments.store'))
+            ->class('p-4 bg-white rounded-md space-y-2')
+            ->fields([
+                Input::make('name')->label('Name'),
+                Submit::make()->label('Save'),
+            ]);
+
+        return view('admin.departments.create', [
+            'form' => $form,
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateDepartmentRequest $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        Department::create($request->validated());
+        Splade::toast('Department created')->autoDismiss(3);
+        return to_route('admin.departments.index');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Department $department)
     {
-        //
+        $form = SpladeForm::make()
+            ->action(route('admin.departments.update', $department))
+            ->method('PUT')
+            ->class('p-4 bg-white rounded-md space-y-2')
+            ->fields([
+                Input::make('name')->label('Name'),
+                Submit::make()->label('Save'),
+            ])
+            ->fill($department);
+
+        return view('admin.departments.edit', [
+            'form' => $form,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CreateDepartmentRequest $request, Department $department)
     {
-        //
+        $department->update($request->validated());
+        Splade::toast('Department updated')->autoDismiss(3);
+        return to_route('admin.departments.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Department $department)
     {
-        //
+        $department->delete();
+        Splade::toast('Department deleted')->autoDismiss(3);
+        return back();
     }
 }
